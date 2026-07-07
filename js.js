de5d1eb197bd5577709155db1637ba8c84e4af07
bankfat <68319010001@swdtcmail.com>
2026-07-07 12:10:23 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+// เปลี่ยนข้อความ
+function changeText(){
+
+    document.getElementById("text").innerHTML =
+    "Hello JavaScript";
+
+}
+
+// แสดงข้อความ
+function showAlert(){
+
+    alert("ยินดีต้อนรับสู่ JavaScript 👍😁");
+
+}
